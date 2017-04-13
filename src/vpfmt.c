579c9a16d11652c1bc71903bb42eb0764068e507
veficos <veficos@gmail.com>
2017-04-13 16:43:15 +0800

    vpfmt

--- src/vpfmt.c
+++ src/vpfmt.c
@@ -2,9 +2,7 @@
 
 #include "config.h"
 #include "token.h"
-#include "color.h"
 #include "vpfmt.h"
-#include "encoding.h"
 
 
 #ifndef MAX_COLUMN_HEAD
@@ -106,47 +104,5 @@ void vpfmt(FILE *fp, const char *fmt, va_list ap)
         } else {
             putc(*fmt, fp);
         }
-    }
-
-    if (loc != NULL) {
-        __output_with_loc__(fp, loc);
-    }
-
-    if (tok != NULL) {
-        __output_with_loc__(fp, tok->loc);
-    }
-}
-
-
-static inline
-void __output_with_loc__(FILE *fp, source_location_t loc)
-{
-    const char *p;
-    const char *q;
-    size_t step;
-
-    if (loc->column >= MAX_COLUMN_HEAD) {
-        p = loc->row;
-        fprintf(stderr, "   ...");
-        fprintf(stderr, p + loc->column);
-        fprintf(stderr, BRUSH_RED("\n      ^\n"));
-
-    } else {
-        p = loc->row;
-        q = p + (loc->column - 1);
-
-        fprintf(stderr, "%s\n", loc->row);
-
-        for (; p < q;) {
-            step = utf8_rune_size(*p);
-            p += step;
-            if (step > 1) {
-                fputc('  ', stderr);
-            } else {
-                fputc(' ', stderr);
-            }
-        }
-
-        fprintf(stderr, BRUSH_RED("^\n"));
     }
 }
